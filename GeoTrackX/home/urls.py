d919e43885c8333a437d7e435b078fe95fdc76fe
Jyothish9988 <Jyothish787898@gmail.com>
2024-02-14 21:57:18 +0530

    template imported and user login and registration completed

--- GeoTrackX/home/urls.py
+++ GeoTrackX/home/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name=''),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('user_login/', views.user_login, name='user_login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('user_logout/', views.user_logout, name='user_logout'),
 
--- GeoTrackX/home/views.py
+++ GeoTrackX/home/views.py
@@ -1,53 +1,45 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-from .forms import CreateUserForm, LoginForm
-from django.contrib.auth.models import auth
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from .forms import CreateUserForm, LoginForm
+from django.contrib.auth import authenticate, login as auth_login, logout
 
 def homepage(request):
     return render(request, 'home/index.html')
 
-
 def register(request):
-    form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("login")
+    else:
+        form = CreateUserForm()
 
     context = {'registerform': form}
-
     return render(request, 'home/register.html', context=context)
 
-
-def login(request):
-    form = LoginForm()
+def user_login(request):  # Renamed from 'login' to 'user_login'
     if request.method == 'POST':
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
             if user is not None:
-                auth.login(request, user)
+                auth_login(request, user)
                 return redirect("dashboard")
 
-    context = {'loginform': form}
+    else:
+        form = LoginForm()
 
+    context = {'loginform': form}
     return render(request, 'home/login.html', context=context)
 
-
+@login_required(login_url='/user_login')  # Adjusted login URL to 'user_login'
 def user_logout(request):
-    auth.logout(request)
+    logout(request)
     return redirect("")
 
-
-@login_required(login_url='/login')
+@login_required(login_url='/user_login')  # Adjusted login URL to 'user_login'
 def dashboard(request):
     return render(request, 'home/dashboard.html')
-
-# Create your views here.
